b58cde7b0db1489689e5c6220d3964fb62b4b38b
Logan <lgntravis@gmail.com>
2023-01-24 20:10:33 -0700

    Updating Gitignore

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Prize Bot</h1>
 <p>
     Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<a class="btn" href="/about">Sign Up</a>
+<a class="btn" href="/about">About</a>
+
+<a class="btn" href="/dashboard">Dashboard</a>
