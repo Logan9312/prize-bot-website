9e01b4ac7d23bfca1eaba44fd52bd9bb3c8e0949
Logan <lgntravis@gmail.com>
2023-06-20 10:37:26 -0600

    Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,16 @@
 	import '../app.css';
 </script>
 
+<nav class="bg-fuchsia-400 text-purple-700">
+	<ul class="h-10 flex items-center space-x-3 justify-center">
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">About</a></li>
+		<li><a href="/contact">Contact</a></li>
+	</ul>
+</nav>
+
 <slot />
+
+<footer>
+	<p>Footer</p>
+</footer>
