7015abc400d16ce80b42b3d07db3a964df59fdaa
Logan <lgntravis@gmail.com>
2023-06-29 09:38:27 -0600

    Formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
 	import '../app.css';
 </script>
 
-<div class="flex flex-col h-screen">
-	<nav class="bg-fuchsia-400 text-purple-700 h-10">
-		<ul class="flex items-center justify-center space-x-3">
+<div class="flex h-screen flex-col">
+	<nav class="h-10 bg-fuchsia-400 text-purple-700">
+		<ul class="flex h-full items-center justify-center space-x-3 align-middle">
 			<li><a href="/">Home</a></li>
 			<li><a href="/about">About</a></li>
 			<li><a href="/contact">Contact</a></li>
