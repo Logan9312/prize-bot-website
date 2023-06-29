080b1b3f3baa5212b08c98123bf8620f03dba011
Logan <lgntravis@gmail.com>
2023-06-28 22:53:56 -0600

    no progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,20 @@
 	import '../app.css';
 </script>
 
-<nav class="bg-fuchsia-400 text-purple-700">
-	<ul class="h-10 flex items-center space-x-3 justify-center">
-		<li><a href="/">Home</a></li>
-		<li><a href="/about">About</a></li>
-		<li><a href="/contact">Contact</a></li>
-	</ul>
-</nav>
+<div class="flex flex-col h-screen">
+	<nav class="bg-fuchsia-400 text-purple-700 h-10">
+		<ul class="flex items-center justify-center space-x-3">
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</nav>
 
-<slot />
+	<main class="flex-grow bg-gray-200">
+		<slot />
+	</main>
 
-<footer>
-	<p>Footer</p>
-</footer>
+	<footer class="bg-fuchsia-400">
+		<p>Footer</p>
+	</footer>
+</div>
