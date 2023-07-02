d953d496c05aa3b3e72cd7a854729c0eaea1ccc3
Logan <lgntravis@gmail.com>
2023-07-01 19:42:59 -0600

    Getting rid of next

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="min-w-full bg-gray-500 pb-6 pt-8">
-      <div>testing</div>
-    </footer>
-  );
-}
