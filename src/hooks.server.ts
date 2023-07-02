d930babc822f3fd24a83239a1994ca997fb6941f
Logan <lgntravis@gmail.com>
2023-07-01 19:46:47 -0600

    More destruction

--- src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import { SvelteKitAuth } from '@auth/sveltekit';
-import Discord from '@auth/core/providers/discord';
-
-import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from '$env/static/private';
-
-export const handle = SvelteKitAuth({
-    //@ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
-	providers: [Discord({ clientId: DISCORD_CLIENT_ID, clientSecret: DISCORD_CLIENT_SECRET })]
-});
