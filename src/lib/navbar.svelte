ea387720223135cd88630940e83744c85a40fe7a
Logan9312 <lgntravis@gmail.com>
2023-07-03 17:50:31 -0600

    CSS template

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <nav>
-	<div class="flex flex-row items-center align-middle py-2 bg-fuchsia-600">
-		<img src={logo} alt="logo" class="mx-5 h-10 w-10" />
+	<div class="flex flex-row items-center bg-fuchsia-600 py-2 align-middle">
+		<a href="/"><img src={logo} alt="logo" class="mx-5 h-10 w-10" /></a>
 		<ul class="flex flex-row space-x-5">
 			<li><a href="/">Home</a></li>
 			<li><a href="/About">About</a></li>
