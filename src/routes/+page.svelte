db14e1530a054b50de7e3edc698bf01b1645acc0
Logan9312 <lgntravis@gmail.com>
2023-07-05 18:58:14 -0600

    Formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { signIn, signOut } from '@auth/sveltekit/client';
 	import { page } from '$app/stores';
 	import Discord from '$lib/assets/discord_logo.svg';
+	import Example from '$lib/assets/example.png';
 </script>
 
 <div class="bg-white">
@@ -15,8 +16,8 @@
 				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
 			/>
 		</div>
-		<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-			<div class="text-center">
+		<div class=" flex max-w-6xl flex-row py-32 sm:py-48 lg:py-56">
+			<div class="text-left">
 				<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
 					Community Rewards Made Easy
 				</h1>
@@ -24,7 +25,7 @@
 					Prize Bot helps you manage your prizes so that you can focus on running quality events for
 					your members and save time tracking the winners.
 				</p>
-				<div class="mt-10 flex items-center justify-center gap-x-6">
+				<div class="mt-10 flex items-center gap-x-6">
 					<a
 						href="https://discord.com/api/oauth2/authorize?client_id=995022149226082324&permissions=8&scope=bot"
 						class="flex rounded-md bg-fuchsia-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-fuchsia-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -35,6 +36,9 @@
 						Learn more <span aria-hidden="true">→</span></a
 					>
 				</div>
+			</div>
+			<div class="mx-10">
+				<img src={Example} alt="Prize Bot Example" class="rounded-xl" />
 			</div>
 		</div>
 		<div
